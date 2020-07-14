ec98c537b17fff70c67d6edf1c962df5525a0293
Mikaël Le Bris <mikgitlab@gmail.com>
2020-07-14 23:17:10 +0200

    Adding communication between microservices. Adding externalisation of configuration files.

--- clientui/src/main/java/com/clientui/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/clientui/ClientUiApplication.java
@@ -2,9 +2,11 @@ package com.clientui.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableFeignClients("com.clientui.clientui")
 public class ClientUiApplication {
 
